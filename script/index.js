4128836cafe19a3bcc219d64c80e94599a6bd323
Andrey <samo.dur.andre@gmail.com>
2022-08-29 10:29:24 +0300

    feat: end project, УРА

--- script/index.js
+++ script/index.js
@@ -11,10 +11,12 @@ const ancientImg = document.querySelector('.ancient__img');
 const stackImg = document.querySelector('.setap__stack-img');
 
 const btnBlock = document.querySelector('.main__button');
-const btnСhoiceAncient = document.querySelector('.btnСhoiceAncient');
+const btnMix = document.querySelector('.btnMix');
+const btnLevel = document.querySelectorAll('.button__link_small');
 const btnPrev = document.querySelector('.btnPrev');
 const titleDeck = document.querySelector('.title-deck');
 
+const titleCount = document.querySelector('.level-title');
 const stageRow = document.querySelectorAll('.general-stage');
 
 const greenStage1 = document.querySelector('.counter__green-stage1');
@@ -32,10 +34,12 @@ const blueStage3 = document.querySelector('.counter__blue-stage3');
 const cardDeck = [];
 
 let ancientId = 'Not ID';
+let idLevel = 'Not selected';
 
 // сохраняем в lokalStorage
 function setLocalStorigesId() {
 	localStorage.setItem('nameAncient', ancientId);
+	localStorage.setItem('stageLevel', idLevel);
 }
 
 // получаем древнего из lokalStorage
@@ -46,11 +50,26 @@ function getLocalStorigesId() {
 
 let ancientLocalStId = getLocalStorigesId();
 
+// получаем уровень сложности из lokalStorage
+function getLocalLevel() {
+	let stageLevel = localStorage.getItem('stageLevel');
+	return stageLevel;
+}
+
+let stageLevel = getLocalLevel();
+
 
 // отключаем кнопку выбрать до выбора древнего
-function chekButton() {
-	btnСhoiceAncient.removeAttribute('disabled');
-	btnСhoiceAncient.classList.remove('disabled');
+function chekButtonLevel() {
+	btnLevel.forEach(elem => {
+		elem.removeAttribute('disabled');
+		elem.classList.remove('disabled');
+	})
+}
+
+function chekButtonMix() {
+	btnMix.removeAttribute('disabled');
+	btnMix.classList.remove('disabled');
 }
 
 // функция для стилей при выборе карточек
@@ -63,7 +82,7 @@ function addRemoveFocus() {
 			});
 			elem.classList.add('focus');
 			elem.classList.remove('antifocus')
-			chekButton();
+			chekButtonLevel();
 		})
 	})
 }
@@ -84,6 +103,37 @@ function getIdAncients() {
 
 getIdAncients();
 
+// функция для стилей при выборе уровня
+function addRemoveFocusLevel() {
+	btnLevel.forEach((elem) => {
+		elem.addEventListener('click', () => {
+			btnLevel.forEach(el => {
+				el.classList.remove('focus');
+			});
+			elem.classList.add('focus');
+		})
+	})
+}
+
+addRemoveFocusLevel();
+
+
+// получаем информацию о уровне сложности
+function choiceLevel() {
+	btnLevel.forEach(elem => {
+		elem.addEventListener('click', (ev) => {
+			idLevel = ev.target.id;
+			ev.target.classList.add('focus')
+			chekButtonMix();
+			setLocalStorigesId();
+		})
+	});
+
+	return idLevel;
+}
+
+choiceLevel();
+
 // перемешиваение карт
 function randomaserDeck(deck) {
 	for (let i = deck.length - 1; i > 0; i--) {
@@ -100,8 +150,27 @@ function creatSubDeck(iter, arr1, arr2) {
 	}
 }
 
-// замешиваем колоду
-function stageCardsById(id) {
+// замешивание карт
+function creatCardsDeck(id, value) {
+
+	let newDeckGreen = cardsDataGreen;
+	let newDeckBrown = cardsDataBrown;
+	let newDeckBlue = cardsDataBlue;
+
+	let level = '';
+
+	if (value === 'easy') {
+		level = 'hard';
+	} else if (value === 'hard') {
+		level = 'easy';
+	}
+
+	if (level === 'hard' || level === 'easy') {
+		newDeckGreen = cardsDataGreen.filter((elem) => elem.difficulty !== level);
+		newDeckBrown = cardsDataBrown.filter((elem) => elem.difficulty !== level);
+		newDeckBlue = cardsDataBlue.filter((elem) => elem.difficulty !== level);
+	}
+
 	const ancient = ancientsData.filter(({ name }) => name === id)[0];
 	const stages = {};
 
@@ -113,35 +182,52 @@ function stageCardsById(id) {
 		const newArr = [];
 		for (const [type, num] of Object.entries(stage)) {
 			if (type.startsWith('green')) {
-				randomaserDeck(cardsDataGreen); // перемешали колоду с зелеными картами
-				creatSubDeck(num, cardsDataGreen, newArr); // собрали нужное зеленных кол-во карт
+				randomaserDeck(newDeckGreen); // перемешали колоду с зелеными картами
+				creatSubDeck(num, newDeckGreen, newArr); // собрали нужное зеленных кол-во карт
 			}
 			if (type.startsWith('brown')) {
-				randomaserDeck(cardsDataBrown);
-				creatSubDeck(num, cardsDataBrown, newArr);
+				randomaserDeck(newDeckBrown);
+				creatSubDeck(num, newDeckBrown, newArr);
 			}
 			if (type.startsWith('blue')) {
-				randomaserDeck(cardsDataBlue);
-				creatSubDeck(num, cardsDataBlue, newArr);
+				randomaserDeck(newDeckBlue);
+				creatSubDeck(num, newDeckBlue, newArr);
 			}
 		}
 
 		randomaserDeck(newArr); // перемешали получившуюся колоду для этапа
 		cardDeck.push(newArr); // засунули колоду в массив
 	}
-
+	console.log(cardDeck)
 	return cardDeck;
 }
 
 // кнопки получения объекта с инфой о нужных карточках, удаление ненужных карточек и перезагрузка страницы
-if (btnСhoiceAncient) {
-	btnСhoiceAncient.addEventListener('click', choiceBtn);
-	btnСhoiceAncient.addEventListener('click', removeCard);
+if (btnMix) {
+	btnMix.addEventListener('click', choiceBtn);
 }
 
 // собираем массив из карточек и передаем на след страницу, происходит по нажатию кнопки "ЗАМЕШАТЬ"
 function choiceBtn() {
-	stageCardsById(ancientId); // функция в которой создаются все три колоды и складываются в одну общую колоду
+	creatCardsDeck(ancientId, idLevel);
+
+	// удаление ненужных карточек
+	function removeCard() {
+		btnBlock.classList.add('down');
+		ancientCard.forEach((elem) => {
+			elem.classList.add('up');
+			makeTransition();
+		})
+	}
+
+	removeCard()
+
+	// отложить переход на след. страничку чтобы отыграла анимация удаления ненужных карточек
+	function makeTransition() {
+		setTimeout(() => {
+			window.location.href = 'next.html';
+		}, 300);
+	}
 
 	localStorage.setItem('deck', JSON.stringify(cardDeck)); // сохранил в localStorage всю стопку для переноса на след. страницу
 	return cardDeck;
@@ -157,22 +243,6 @@ function addInCardNextPage() {
 
 addInCardNextPage();
 
-// удаление ненужных карточек
-function removeCard() {
-	btnBlock.classList.add('down');
-	ancientCard.forEach((elem) => {
-		elem.classList.add('up');
-		makeTransition();
-	})
-}
-
-// отложить переход на след. страничку чтобы отыграла анимация удаления ненужных карточек
-function makeTransition() {
-	setTimeout(() => {
-		window.location.href = 'next.html';
-	}, 300);
-}
-
 // работа с колодой и показ данных о колодах
 function schowDeck() {
 	// достаем из local storiges массив с картами
@@ -183,12 +253,14 @@ function schowDeck() {
 	}
 
 	// для кроссчека
+	console.log(`------- ${stageLevel} level -------\n\n`);
+	console.log(`------- карты можно открывать кликая мышкой (есть специальная задержка) или нажимая на пробел -------\n------- когда колода закончилась, кнопка "замешать ещё" доступна и по нажатию на enter -------\n\n`);
 	console.log(newDeckCard[0]);
-	console.log('------- deck of cards for stage 1 -------\n');
+	console.log(`------- deck of cards for stage 1 -------\n`);
 	console.log(newDeckCard[1]);
-	console.log('------- deck of cards for stage 2 -------\n');
+	console.log(`------- deck of cards for stage 2 -------\n`);
 	console.log(newDeckCard[2])
-	console.log('------- deck of cards for stage 3 -------\n');
+	console.log(`------- deck of cards for stage 3 -------\n`);
 	//
 
 	// считаем карточки по цветам
@@ -203,7 +275,26 @@ function schowDeck() {
 		return countNumber;
 	}
 
-	// отображение в счетчике
+	// формируем заголовок
+	function getTitleCount() {
+		const level = {
+			easy: 'прогулка',
+			normal: 'классика',
+			hard: 'будет тяжко',
+		}
+
+		for (let key in level) {
+			if (key === stageLevel) {
+				titleCount.innerHTML = `Сложность «${level[key]}»`;
+			}
+		}
+	}
+
+	if (titleCount) {
+		getTitleCount();
+	}
+
+	//	отображение в счетчике
 	function showCountNum() {
 		if (greenStage1, greenStage2, greenStage3) {
 			countCard('green');
@@ -229,11 +320,17 @@ function schowDeck() {
 
 	document.addEventListener("DOMContentLoaded", showCountNum); // отображение кол-ва карт в счетчике при загрузки страницы
 
-
-
 	// открываем карточки из стопки
 	if (stackImg) {
 		stackImg.addEventListener('click', openCard);
+
+		document.addEventListener('keydown', (ev) => {
+			if (ev.code === 'Space') {
+				if (newDeckCard[2].length) {
+					openCard();
+				}
+			}
+		})
 	}
 
 	function setDellayBtnOPenCard() {
@@ -244,8 +341,6 @@ function schowDeck() {
 		}, 500)
 	}
 
-
-
 	// берем карты из массива и показываем их на экран
 	let i = 0;
 
@@ -253,8 +348,8 @@ function schowDeck() {
 		if (newDeckCard[i].length || i++) {
 			let card = newDeckCard[i].pop();
 			stackImg.src = card.cardFace;
-
-			console.log(`open card: ${card.id}`);
+			// для кроссчека
+			console.log(`${card.id}, ${card.difficulty}`);
 		}
 
 		showCountNum();
@@ -262,8 +357,6 @@ function schowDeck() {
 		changeBtnPrev();
 		setDellayBtnOPenCard();
 	}
-
-
 
 	// по окончанию карт в этапе делаем строку "неактивной"
 	function arrStringCount() {
@@ -280,6 +373,12 @@ function schowDeck() {
 			stackImg.remove();
 			btnPrev.innerHTML = 'Замешать ещё';
 			titleDeck.innerHTML = 'Карты закончились';
+
+			document.addEventListener('keydown', (ev) => {
+				if (ev.code === 'Enter') {
+					window.location.href = 'index.html';
+				}
+			})
 		}
 	}
 
